c25f90a94ecce66155fd9083b51f05206717afd5
MASurganova <masurganova@gmail.com>
2018-11-14 16:01:41 +0300

    add controllers tests

---
+++ src/test/java/ru/voting/web/RootControllerTest.java
@@ -0,0 +1,30 @@
+package ru.voting.web;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static ru.voting.TestData.USER;
+import static ru.voting.TestData.USER_ID;
+import static ru.voting.model.AbstractBaseEntity.START_SEQ;
+
+public class RootControllerTest extends AbstractControllerTest {
+
+    @Test
+    public void testUsers() throws Exception {
+        mockMvc.perform(get("/users"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("users"))
+                .andExpect(forwardedUrl("/WEB-INF/jsp/users.jsp"))
+                .andExpect(model().attribute("users", hasSize(2)))
+                .andExpect(model().attribute("users", hasItem(
+                        allOf(
+                                hasProperty("id", is(USER_ID)),
+                                hasProperty("name", is(USER.getName()))
+                        )
+                )));
+    }
+}
