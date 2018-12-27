8f637fabe8e58e701c034c8bb39c802fa2299e56
MASurganova <masurganova@gmail.com>
2018-12-27 22:09:53 +0300

    add lost UserServiceImpl

---
+++ src/main/java/ru/voting/service/UserServiceImpl.java
@@ -0,0 +1,90 @@
+package ru.voting.service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
+import ru.voting.AuthorizedUser;
+import ru.voting.model.User;
+import ru.voting.repository.UserRepository;
+import ru.voting.to.UserTo;
+import ru.voting.util.ValidationUtil;
+import ru.voting.util.exception.NotFoundException;
+
+import java.util.List;
+
+import static ru.voting.util.UserUtil.updateFromTo;
+import static ru.voting.util.ValidationUtil.checkNotFoundWithId;
+
+@Service("userService")
+public class UserServiceImpl implements UserService, UserDetailsService {
+
+    private final UserRepository repository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @CacheEvict(value = "users", allEntries = true)
+    @Override
+    public void delete(int id) throws NotFoundException {
+        ValidationUtil.checkNotFoundWithId(repository.delete(id), id);
+    }
+
+    @Override
+    public User get(int id) throws NotFoundException {
+        return ValidationUtil.checkNotFoundWithId(repository.get(id), id);
+    }
+
+    @Cacheable("users")
+    @Override
+    public List<User> getAll() {
+        return repository.getAll();
+    }
+
+    @Override
+    public User getByEmail(String email) throws NotFoundException {
+        return ValidationUtil.checkNotFound(repository.getByEmail(email), "email=" + email);
+    }
+
+    @CacheEvict(value = "users", allEntries = true)
+    @Override
+    public void update(User user) throws NotFoundException {
+        Assert.notNull(user, "user must not be null");
+        checkNotFoundWithId(repository.save(user), user.getId());
+    }
+
+    @CacheEvict(value = "users", allEntries = true)
+    @Transactional
+    @Override
+    public void update(UserTo userTo) {
+        User user =  updateFromTo(get(userTo.getId()), userTo);
+        repository.save(user);
+    }
+
+    @CacheEvict(value = "users", allEntries = true)
+    @Override
+    public User create(User  user) {
+        return repository.save(user);
+    }
+
+    @Override
+    public User getWithChoice(int id) { return repository.getWithChoice(id); }
+
+    @Override
+    public AuthorizedUser loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = repository.getByEmail(email.toLowerCase());
+        if (user == null) {
+            throw new UsernameNotFoundException("User " + email + " is not found");
+        }
+        return new AuthorizedUser(user);
+    }
+
+}
